e0fc3e903f8d7b0c44d380be6e1b0ca924677f26
EricNY <ericabt@gmail.com>
2013-11-18 21:07:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NeedABeer::Application.config.secret_key_base = '8bddb73dae642e9688b4dae60a8b185c474fbdde4cf41bf253437b7a48886a398d0768a0c3adb604222b8bb66ff29d9c008754e097273d377ad6c21b5e6b9046'
